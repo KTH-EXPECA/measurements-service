e97a6a987a3f99a74089ce63f8b537183513c321
Samie Mostafavi <ssmos@kth.se>
2022-12-02 10:41:35 +0000

    debug

--- combined/combined-entrypoint.sh
+++ combined/combined-entrypoint.sh
@@ -2,6 +2,6 @@
 while true
 do
 	sh -c 'irtt client -i 10ms -d 100s -l 172 --fill=rand --sfill=rand -o /tmp/results/irtt_data.json 10.99.99.3 & python3 /tmp/ntw-meas.py & wait'
-	sh -c 'python3 upload_files.py /tmp/results/irtt_data.json'
-	sh -c 'python3 upload_files.py /tmp/results/ntw_meas_data.json'
+	sh -c 'python3 /tmp/upload_files.py /tmp/results/irtt_data.json'
+	sh -c 'python3 /tmp/upload_files.py /tmp/results/ntw_meas_data.json'
 done
