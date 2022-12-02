1569c95c06733b4d5ff69e40ab6b2b1ed1fa0533
Samie Mostafavi <ssmos@kth.se>
2022-12-02 10:23:06 +0000

    add combined service infinite loop and upload files to swift

--- combined/combined-entrypoint.sh
+++ combined/combined-entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sh -c 'irtt client -i 10ms -d 100s -l 172 --fill=rand --sfill=rand -o /tmp/results/irtt_data.json 10.99.99.3 & python3 /tmp/ntw-meas.py & wait'
+while true
+do
+	sh -c 'irtt client -i 10ms -d 100s -l 172 --fill=rand --sfill=rand -o /tmp/results/irtt_data.json 10.99.99.3 & python3 /tmp/ntw-meas.py & wait'
+	sh -c 'python3 upload_files.py /tmp/results/irtt_data.json'
+	sh -c 'python3 upload_files.py /tmp/results/ntw_meas_data.json'
+done
