a7b9500b324d54ca2191f8e3a1b5c9d514de6960
Samie Mostafavi <ssmos@kth.se>
2022-12-08 11:36:52 +0000

    get irtt duration, interval, server from env vars

--- combined/combined-entrypoint.sh
+++ combined/combined-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-	sh -c 'irtt client -i 10ms -d 100s -l 172 --fill=rand --sfill=rand -o /tmp/results/irtt_data.json 10.99.99.3 & python3 /tmp/ntw-meas.py & wait'
+	sh -c 'irtt client -i $IRTT_INTERVAL -d $IRTT_DURATION -l $IRTT_LENGTH --fill=rand --sfill=rand -o /tmp/results/irtt_data.json $IRTT_SERVER_IP & python3 /tmp/ntw-meas.py & wait'
 	sh -c 'python3 /tmp/upload_files.py /tmp/results/irtt_data.json'
 	sh -c 'python3 /tmp/upload_files.py /tmp/results/ntw_meas_data.json'
 done
