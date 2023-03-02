fa319ae9f2897bfaa018210a2a89498b7a488fae
samiemostafavi <samiemostafavi@gmail.com>
2023-03-02 19:55:14 +0100

    debug irtt server

--- server/server-entrypoint.sh
+++ server/server-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sh -c 'irtt server -i $IRTT_INTERVAL -d $IRTT_DURATION -l $IRTT_LENGTH --fill=rand --sfill=rand -b $IRTT_BIND_ADDR'
+sh -c 'irtt server -i $IRTT_INTERVAL -d $IRTT_DURATION -l $IRTT_LENGTH -b $IRTT_BIND_ADDR'
